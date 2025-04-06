d7c7a9c0d7c143a3de094f06b95a6c935ea2c728
katayama8000 <tattu.0310@gmail.com>
2025-04-06 13:18:33 +0000

    Refactor movie and review models to establish relationships and improve structure

--- myapp/src/models/reviews.rs
+++ myapp/src/models/reviews.rs
@@ -1,5 +1,5 @@
+pub use super::_entities::reviews::{ActiveModel, Entity, Model};
 use sea_orm::entity::prelude::*;
-pub use super::_entities::reviews::{ActiveModel, Model, Entity};
 pub type Reviews = Entity;
 
 #[async_trait::async_trait]
